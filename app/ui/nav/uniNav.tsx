def21ccf99aeadf8672da54e2bb05e59b6cd9388
Sunight00 <Soleil@123#>
2026-06-16 02:31:11 +0100

    okay

--- app/ui/nav/uniNav.tsx
+++ app/ui/nav/uniNav.tsx
@@ -2,10 +2,15 @@
 
 import Image from 'next/image';
 import Profile from './profile';
-export default function UniNav() {
+import { auth } from '@/auth';
+
+export default async function UniNav() {
+  const session = await auth();
+  const user = session?.user;
   return (
     <nav className="p-4 border-b border-gray-300 bg-white shadow-md flex items-center justify-between">
         <Image src="/medical-symbol.png" alt="Medical Icon" loading="eager" width={40} height={40} className="inline-block mr-2" />
+        <p>{user?.name}</p>
         <Profile />
     </nav>
   );
